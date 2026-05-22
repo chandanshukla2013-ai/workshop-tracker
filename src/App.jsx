d019ac7188d1a40df0fceaf128ce59db5ed5408d
Chandan Shukla <chandanshukla2013@gmail.com>
2026-05-22 12:25:16 +0530

    Initial workshop tracker project

---
+++ src/App.jsx
@@ -0,0 +1,263 @@
+import { useState } from "react";
+
+function App() {
+const [equipmentList, setEquipmentList] = useState([
+  {
+    id: "MTR-001",
+    name: "110kW Pump Motor",
+    location: "1408 Pumping Station",
+    status: "Running"
+  },
+
+  {
+    id: "PMP-002",
+    name: "8CHR Pump",
+    location: "3-Pit Shaft",
+    status: "Under Repair"
+  }
+]);
+
+const [equipmentId, setEquipmentId] = useState("");
+const [equipmentName, setEquipmentName] = useState("");
+const [location, setLocation] = useState("");
+const [status, setStatus] = useState("Running");
+const handleAddEquipment = () => {
+
+  const newEquipment = {
+    id: equipmentId,
+    name: equipmentName,
+    location: location,
+    status: status
+  };
+
+  setEquipmentList([...equipmentList, newEquipment]);
+
+  setEquipmentId("");
+  setEquipmentName("");
+  setLocation("");
+  setStatus("Running");
+};
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+
+      {/* Sidebar */}
+      <div className="w-64 bg-blue-900 text-white p-5">
+
+        <h1 className="text-2xl font-bold mb-10">
+          Workshop Tracker
+        </h1>
+
+        <ul className="space-y-4">
+
+          <li className="bg-blue-700 p-3 rounded-lg">
+            Dashboard
+          </li>
+
+          <li className="hover:bg-blue-700 p-3 rounded-lg cursor-pointer">
+            Equipment
+          </li>
+
+          <li className="hover:bg-blue-700 p-3 rounded-lg cursor-pointer">
+            Repairs
+          </li>
+
+          <li className="hover:bg-blue-700 p-3 rounded-lg cursor-pointer">
+            Reports
+          </li>
+
+        </ul>
+
+      </div>
+
+      {/* Main Content */}
+      <div className="flex-1 p-8">
+
+        <h2 className="text-3xl font-bold mb-8">
+          Dashboard
+        </h2>
+
+        {/* Cards */}
+        {/* Add Equipment Form */}
+
+<div className="bg-white p-6 rounded-xl shadow mb-10">
+
+  <h3 className="text-2xl font-bold mb-6">
+    Add New Equipment
+  </h3>
+
+  <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+
+    <div>
+      <label className="block mb-2 font-semibold">
+        Equipment ID
+      </label>
+
+     <input
+      type="text"
+      placeholder="Enter ID"
+      value={equipmentId}
+      onChange={(e) => setEquipmentId(e.target.value)}
+      className="w-full border p-3 rounded-lg"
+    />
+    </div>
+
+    <div>
+      <label className="block mb-2 font-semibold">
+        Equipment Name
+      </label>
+    <input
+      type="text"
+      placeholder="Enter Equipment Name"
+      value={equipmentName}
+      onChange={(e) => setEquipmentName(e.target.value)}
+      className="w-full border p-3 rounded-lg"
+    />
+
+    </div>
+
+    <div>
+      <label className="block mb-2 font-semibold">
+        Location
+      </label>
+
+    <input
+      type="text"
+      placeholder="Enter Location"
+      value={location}
+      onChange={(e) => setLocation(e.target.value)}
+      className="w-full border p-3 rounded-lg"
+    />
+    </div>
+
+    <div>
+      <label className="block mb-2 font-semibold">
+        Status
+      </label>
+
+    <select
+      value={status}
+      onChange={(e) => setStatus(e.target.value)}
+      className="w-full border p-3 rounded-lg"
+    >
+
+        <option>Running</option>
+        <option>Under Repair</option>
+        <option>Standby</option>
+
+      </select>
+    </div>
+
+  </div>
+
+ <button
+  onClick={handleAddEquipment}
+  className="mt-6 bg-green-600 text-white px-6 py-3 rounded-lg hover:bg-green-700"
+ >
+
+    Save Equipment
+
+  </button>
+
+</div>
+        {/* Equipment Table */}
+
+<div className="bg-white mt-10 p-6 rounded-xl shadow">
+
+<div className="flex justify-between items-center mb-6">
+
+  <h3 className="text-2xl font-bold">
+    Equipment List
+  </h3>
+
+  <button className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">
+    Add Equipment
+  </button>
+
+</div>
+
+  <table className="w-full border-collapse">
+
+    <thead>
+
+      <tr className="bg-gray-200">
+
+        <th className="text-left p-3">ID</th>
+        <th className="text-left p-3">Equipment</th>
+        <th className="text-left p-3">Location</th>
+        <th className="text-left p-3">Status</th>
+
+      </tr>
+
+    </thead>
+
+    <tbody>
+      {equipmentList.map((item, index) => (
+
+  <tr key={index} className="border-b">
+
+    <td className="p-3">
+      {item.id}
+    </td>
+
+    <td className="p-3">
+      {item.name}
+    </td>
+
+    <td className="p-3">
+      {item.location}
+    </td>
+
+    <td className="p-3 font-semibold">
+      {item.status}
+    </td>
+
+  </tr>
+
+))}
+
+    </tbody>
+
+  </table>
+
+</div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+
+          <div className="bg-white p-6 rounded-xl shadow">
+            <h3 className="text-lg font-semibold">
+              Total Equipment
+            </h3>
+
+            <p className="text-5xl font-bold text-blue-600 mt-4">
+              120
+            </p>
+          </div>
+
+          <div className="bg-white p-6 rounded-xl shadow">
+            <h3 className="text-lg font-semibold">
+              Under Repair
+            </h3>
+
+            <p className="text-5xl font-bold text-red-500 mt-4">
+              15
+            </p>
+          </div>
+
+          <div className="bg-white p-6 rounded-xl shadow">
+            <h3 className="text-lg font-semibold">
+              Available
+            </h3>
+
+            <p className="text-5xl font-bold text-green-600 mt-4">
+              85
+            </p>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
